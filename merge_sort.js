208392b66fdb8eedb3933e05346ed1a1388f53ac
Jay <jarezvani@gmail.com>
2016-08-06 21:52:11 -0700

    Create merge_sort.js

---
+++ merge_sort.js
@@ -0,0 +1,38 @@
+function mergeSort(a){    
+  if(a.length<2)
+    return a;
+  
+  var middle = Math.floor(a.length/2),
+      left = a.slice(0, middle),
+      right = a.slice(middle, a.length);
+
+  
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right){
+  var newArr = [];
+  
+  if(!c){
+    var c = 0;
+  }
+  
+  while(left.length && right.length){
+    if(left[0] <= right[0]){
+      newArr.push(left.shift());
+    }else{
+      newArr.push(right.shift());
+      c++;
+    }
+  }
+  
+  while(left.length){
+    newArr.push(left.shift());
+  }
+  
+  while(right.length){
+    newArr.push(right.shift());
+  }
+  
+  return c;
+}
